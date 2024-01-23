9b1626450105ef42a34de0315aa593468b06633e
CLNAE <148075440+CLNAE@users.noreply.github.com>
2024-01-24 00:08:56 +0200

    v1.0
    Program finalizat.

---
+++ Backend.py
@@ -0,0 +1,95 @@
+#importarea librariilor folosite in program
+import json
+from datetime import date
+import calendar
+import os
+import PySimpleGUI as sg
+
+#definirea claselor proiectului
+class Orar:
+    #constructorul care ofera valori default clasei la instantiere
+    def __init__(self):
+        self.ore = "Alege saptamana"
+        self.dataAzi = date.today()
+        self.numeZi = calendar.day_name[self.dataAzi.weekday()]
+
+    #metodele clasei Orar
+    def afisareOrar_par(self, window):
+        with open('orar_par.json') as fisjson1:
+            orar = json.load(fisjson1)
+            ore = orar[self.numeZi]
+            window['textorar'].update(ore)
+            return (ore)
+
+    def afisareOrar_impar(self, window):
+        with open('orar_impar.json') as fisjson2:
+            orar = json.load(fisjson2)
+            ore = orar[self.numeZi]
+            window['textorar'].update(ore)
+            return (ore)
+
+
+class Listbox:
+    # constructorul care ofera valori default clasei la instantiere
+    def __init__(self):
+        self.dirCurent = os.path.dirname(os.path.abspath(__file__))
+        self.fisiere = [f for f in os.listdir(self.dirCurent) if f.endswith('.txt')]
+
+    # metodele clasei Listbox
+    def functieStergere(self, values, window):
+        if values['listaFisiere']:
+            fisierSelectat = values['listaFisiere'][0]
+            if sg.popup_ok_cancel(f'Esti sigur ca vrei sa stergi {fisierSelectat}?', title="Atentie!",
+                                  keep_on_top=True) == 'OK':
+                os.remove(os.path.join(self.dirCurent, fisierSelectat))
+                sg.popup('Fisierul a fost sters!', keep_on_top=True)
+                self.fisiere = [f for f in os.listdir(self.dirCurent) if f.endswith('.txt')]
+                window['listaFisiere'].update(self.fisiere)
+
+    def deschidereFisier(self, values, window):
+        if values['listaFisiere']:
+            fisierSelectat = values['listaFisiere'][0]
+            with open(fisierSelectat, 'r') as lectie:
+                self.TextLectie = lectie.read()
+                self.titluLectie = fisierSelectat
+                print(self.titluLectie)
+                window['numeLectieDeLaUser'].update(self.titluLectie)
+                window['txtLectie'].update(self.TextLectie)
+                return self.titluLectie, self.TextLectie
+
+class Multiline:
+    # constructorul care ofera valori default clasei la instantiere
+    def __init__(self, obiectListbox):
+        self.obiectListbox = obiectListbox
+        self.TextLectie = (
+        "    Bine ai (re)venit la organizatorul tau!\n\n    Daca este prima data cand folosesti aceasta aplicatie"
+        ", ti-am pregatit o mica introducere insotita de un set de instructiuni de utilizare.\n\n    Tot ce trebuie sa"
+        " faci pentru a accesa aceasta introducere este sa mergi la lista din dreapta, sa dai click pe fisierul "
+        "Instructiuni.txt, iar mai apoi sa apesi butonul Deschide.")
+        self.titluLectie = ''
+
+    # metodele clasei Multiline
+    def functieGolire(self, window):
+        self.TextLectie = ''
+        window['txtLectie'].update(self.TextLectie)
+
+    def functieSalvare(self, values, window):
+        titlu = values['numeLectieDeLaUser']
+        text = values['txtLectie']
+        path = obiectListbox.dirCurent + os.sep + titlu
+        os.path.abspath(path)
+        print(path)
+        if os.path.isfile(path) == False or (os.path.isfile(path) == True and
+                                             sg.popup_ok_cancel('Exista deja un fisier cu acelasi nume.',
+                                                                'Esti sigur ca vrei sa il suprascrii?',
+                                                                title="Atentie!", keep_on_top=True) == 'OK'):
+            print(titlu)
+            with open(titlu, 'w') as fisSel:
+                fisSel.write(text)
+        obiectListbox.fisiere = [f for f in os.listdir(obiectListbox.dirCurent) if f.endswith('.txt')]
+        window['listaFisiere'].update(obiectListbox.fisiere)
+
+#instantierea claselor
+obiectOrar = Orar()
+obiectListbox = Listbox()
+obiectMultiline = Multiline(Listbox())
---
+++ Frontend.py
@@ -0,0 +1,71 @@
+#importam toata partea de backend (variablie, clase, functii, instante)
+from Backend import *
+
+#setari pentru aspectul general al programului
+sg.set_options(font=('Courier',12))
+sg.theme('DarkGrey5')
+
+#delimitarea elementelor layout-ului pe 3 coloane: stanga, centru, dreapta
+stanga = [
+    [sg.Push(), sg.Text('Orar\n\n', font=('Times New Roman', 20), text_color='navajo white'), sg.VPush()],
+    [sg.Push(), sg.Text('Saptamana: ',font=('Courier',16)),sg.Combo(['para','impara'], readonly=True,
+    key='COMBO'),sg.VPush()],
+    [sg.Push(), sg.Text(obiectOrar.ore, justification='center', key='textorar'), sg.Push()],
+    [sg.VPush(), sg.Push(), sg.Button('Afiseaza'), sg.Push(), sg.VPush()]
+]
+
+centru = [
+    [sg.Push(), sg.Text('Titlul lectiei:', ), sg.Input(obiectMultiline.titluLectie, size=30, key='numeLectieDeLaUser',
+                                                       do_not_clear=False), sg.Push()],
+    [sg.Multiline(obiectMultiline.TextLectie, key="txtLectie", size=(80, 30))],
+    [sg.Button('Salveaza Lectia' ), sg.Push(), sg.Button('Goleste')]
+]
+
+dreapta = [
+    [sg.Push(), sg.Text('Lista lectii', font=('Times New Roman', 20), text_color='navajo white'), sg.Push()],
+    [sg.Listbox(values=obiectListbox.fisiere, size=(25, 25), key='listaFisiere')],
+    [sg.Button('Deschide', size=(10, 1), ), sg.Push(), sg.Button('Sterge', size=(10, 1))]
+]
+
+#constructia layout-ului
+layout = [
+    [sg.Push(), sg.Text('ORGANIZATOR', font=('Times New Roman',20), text_color='navajo white'), sg.Push(),
+     sg.Button('Exit')],
+    [sg.Column(stanga), sg.Column(centru), sg.Column(dreapta)],
+    [sg.Text('© Călin Andrei-Emil', font=('Times New Roman', 10), text_color='navajo white'), sg.Push()]
+    ]
+
+# Creeaza Window-ul. Finalize permite window-ului sa isi ia update.
+window = sg.Window('Organizator', layout, size=(1500, 700), enable_close_attempted_event=True, finalize=True)
+
+# Loop care tine window-ul deschis, il updateaza si pune conditia de inchidere
+while True:
+    event, values = window.read()
+
+    if ((event == sg.WINDOW_CLOSE_ATTEMPTED_EVENT or event == 'Exit') and
+        sg.popup_ok_cancel("     Esti sigur ca vrei sa iesi?", " Asigura-te ca ai salvat totul inainte.",
+                           title="Atentie!", keep_on_top=True) == 'OK'):
+        break
+
+    if values['COMBO'] == 'impara':
+        ore = obiectOrar.afisareOrar_impar(window)
+    elif values['COMBO'] == 'para':
+        ore = obiectOrar.afisareOrar_par(window)
+
+    if event == 'Afiseaza':
+        window['textorar'].update(ore)
+
+    if event == 'Salveaza Lectia':
+        obiectMultiline.functieSalvare(values, window)
+
+    if event == 'Goleste':
+        obiectMultiline.functieGolire(window)
+
+    if event == 'Deschide':
+        obiectListbox.deschidereFisier(values, window)
+
+    if event == 'Sterge':
+        obiectListbox.functieStergere(values, window)
+
+#inchide window-ul
+window.close()
